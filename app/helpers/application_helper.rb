e54b3c4ae03cc7db91a940f759f4fcfc70f0d580
Sal Olivares <git@salolivares.com>
2024-01-06 00:49:32 -0800

    css woo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def active_link_to(text, path, base_classes: "", active_classes: "", additional_classes: "")
+    # Determine if the current page is the same as the link's path
+    active = current_page?(path) ? active_classes : ""
+
+    # Combine all classes
+    all_classes = [base_classes, additional_classes, active].join(' ').strip
+
+    # Return the link_to method with the combined classes
+    link_to text, path, class: all_classes
+  end
 end
