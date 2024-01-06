7b4fbf8c80dc81f79299847596eb038ad953ee8b
Sal Olivares <git@salolivares.com>
2024-01-05 23:26:59 -0800

    stimulus magic

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus";
+import { useClickOutside } from "stimulus-use";
 
 // Connects to data-controller="dropdown"
 export default class extends Controller {
@@ -7,14 +8,42 @@ export default class extends Controller {
   static values = { initialActive: { type: Boolean, default: false } };
 
   connect() {
+    useClickOutside(this);
+
     if (this.initialActiveValue === true) {
       // use the initialActiveValue passed from data-dropdown-initial-active-value to optionally toggle our class
       this.toggle();
     }
   }
 
+  clickOutside(event) {
+    this.close();
+  }
+
+  closeWithKeyboard(event) {
+    if (event.key === "Escape") {
+      this.close();
+    }
+  }
+
+  closeOnBigScreen(event) {
+    if (window.innerWidth > 768) {
+      this.close();
+    }
+  }
+
   toggle() {
+    console.log("hit");
     this.menuClasses.map((c) => this.menuTarget.classList.toggle(c));
     this.buttonClasses.map((c) => this.buttonTarget.classList.toggle(c));
+  }
+
+  close() {
+    this.menuClasses.map((c) => this.menuTarget.classList.add(c));
+    this.buttonClasses.map((c) => this.buttonTarget.classList.remove(c));
+    // let main = document.querySelector("main")
+    // main.classList.remove("blur")
+    // document.body.classList.remove("overflow-hidden");
+    // main.classList.remove("hidden")
   }
 }
