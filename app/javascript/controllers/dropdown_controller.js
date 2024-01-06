e02988cf2ed98efeead13a768e11758612f292ed
Sal Olivares <git@salolivares.com>
2024-01-06 00:04:33 -0800

    stupid complexity

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -4,11 +4,23 @@ import { useClickOutside } from "stimulus-use";
 // Connects to data-controller="dropdown"
 export default class extends Controller {
   static classes = ["menu", "button"];
-  static targets = ["menu", "button"];
-  static values = { initialActive: { type: Boolean, default: false } };
+  static targets = ["menu", "button", "visibility"];
+  static values = {
+    initialActive: { type: Boolean, default: false },
+    visibility: { type: String, default: "block" },
+  };
 
   connect() {
     useClickOutside(this);
+
+    // save initial visibility states, hashmap of index and visibility value created from this.visibilityTargets
+    this.initialVisibilityState = new Map();
+    this.visibilityTargets.map((t, index) =>
+      this.initialVisibilityState.set(
+        index,
+        t.classList.contains("hidden") ? "hidden" : this.visibilityValue
+      )
+    );
 
     if (this.initialActiveValue === true) {
       // use the initialActiveValue passed from data-dropdown-initial-active-value to optionally toggle our class
@@ -34,6 +46,16 @@ export default class extends Controller {
     this.buttonClasses.map((c) =>
       this.buttonTargets.map((t) => t.classList.toggle(c))
     );
+
+    this.visibilityTargets.map((t) => {
+      if (t.classList.contains("hidden")) {
+        t.classList.remove("hidden");
+        t.classList.add(this.visibilityValue);
+      } else {
+        t.classList.add("hidden");
+        t.classList.remove(this.visibilityValue);
+      }
+    });
   }
 
   close() {
@@ -44,5 +66,11 @@ export default class extends Controller {
     this.buttonClasses.map((c) =>
       this.buttonTargets.map((t) => t.classList.remove(c))
     );
+
+    this.visibilityTargets.map((t, index) => {
+      t.classList.remove(this.visibilityValue);
+      t.classList.remove("hidden");
+      t.classList.add(this.initialVisibilityState.get(index));
+    });
   }
 }
